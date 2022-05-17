eb0f16ce49d4e90681f7296e4fa083d9350e3ff0
KoAsH08 <96475939+KoAsH08@users.noreply.github.com>
2022-05-17 21:05:55 +0100

    Update streamlit_app.py
    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Idly')
+streamlit.text('Dosa')
+streamlit.text('Pongal')
+streamlit.text('Tea')
