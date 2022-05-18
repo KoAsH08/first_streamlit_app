6eafde1d940de40f76c25d23829d45b9b36809fc
KoAsH08 <96475939+KoAsH08@users.noreply.github.com>
2022-05-18 23:01:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('FruityVice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Apple')
-if not fruit choice:
+if not fruit_choice:
      streamlit.write('The user entered', fruit_choice)
 else:
 
